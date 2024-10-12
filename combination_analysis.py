915cab12f5e1a61a9d871c29bf52ff66a3206d2c
Gulzhan-g <gulzhansharapiyeva@gmail.com>
2024-10-12 14:15:07 +0500

    Update combination_analysis.py

--- combination_analysis.py
+++ combination_analysis.py
@@ -5,22 +5,20 @@ from itertools import combinations
 df = pd.read_csv('data/train_1.csv', sep=';', encoding='utf-8')
 df2=df[['Order ID','Product Name',"order_count"]].copy()
 df2 = df2.rename(columns = {"Order ID": "Order_ID","Product Name":"item","order_count":"amount"})
+
 # Find the number of combinations that have bought the most or the least
 # I create a list from the listbox that contains all the products in each of the purchases
 mylist = df2.pivot(index = 'Order_ID',
           columns = 'item',
           values = 'item').values.tolist()
-
 # The list may contain empty values, so I exclude them from the list
 mylist = [[i for i in j if i == i] for j in mylist]
-
 # Use the combinations library to find all pairs of products in each purchase to combine them into a single list
 total = []
 for i in mylist:
     a = (list(combinations(i,2)))
     for b in a:
         total.append(b)
-
 # To find the number of combinations. Forming, grouping and sorting a data frame from a list
 patterns = pd.DataFrame(total,columns = ['item_x', 'item_y'])
 patterns = patterns\
